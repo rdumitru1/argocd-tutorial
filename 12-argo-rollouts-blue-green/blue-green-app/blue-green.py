1808e1c6fa55eb210464177febb6b87309f5fd7b
rmdumitru <113442092+rmdumitru@users.noreply.github.com>
2025-03-26 13:06:30 +0200

    Modifie README file

--- 12-argo-rollouts-blue-green/blue-green-app/blue-green.py
+++
@@ -1,9 +0,0 @@
-from os import environ
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-  return render_template(environ['html_name'])
-if __name__ == '__main__':
-  app.run()
